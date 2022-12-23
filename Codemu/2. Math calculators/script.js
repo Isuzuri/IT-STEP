95bf10b47fb3f4bb3db47afcba6581e00344ec10
Isuzu <ballage94@gmail.com>
2022-12-23 23:06:53 +0300

    codemu 2.3

--- Codemu/2. Math calculators/script.js
+++ Codemu/2. Math calculators/script.js
@@ -28,3 +28,22 @@ something.onclick = function () {
     arr.splice(arr.indexOf(MAX), 1);
     console.log (MAX ** 2 == arr[0] ** 2 + arr[1] ** 2)
 }
+
+findDiv.onclick = function() {
+    let numDiv = +document.getElementById('numDiv').value;
+    for (let i = 0; i <= numDiv; i++) {
+        if (numDiv % i == 0) {
+            console.log(i);
+        } continue;
+    }
+}
+
+sharedDiv.onclick = function() {
+    let num1 = +document.getElementById('num1').value;
+    let num2 = +document.getElementById('num2').value;
+    for (let i = 0; i < Math.max(num1, num2); i++) {
+        if () {
+            console.log(i);
+        } continue;
+    }
+}
