3f7207b706003635c73589415f87159751f32baa
Isuzu <ballage94@gmail.com>
2023-01-16 22:01:17 +0300

    task 6-7 complete

--- Homework 10/script.js
+++ Homework 10/script.js
@@ -63,7 +63,51 @@ function task5Create() {
     createSubmit(5);
 }
 
-// =============   Task 1   ==================== 
+function task6Create() {
+    createDesc('Введи кол-во долларов')
+    createInput()
+    const createButtonDiv = document.createElement('div')
+    createButtonDiv.setAttribute('display', 'flex');
+    showAreaPlace.appendChild(createButtonDiv);
+
+    const createButton1 = document.createElement('button');
+    createButton1.textContent = 'EUR';
+    createButton1.setAttribute('onclick', 'inEur()')
+    createButtonDiv.appendChild(createButton1);
+
+    const createButton2 = document.createElement('button');
+    createButton2.textContent = 'UAH';
+    createButton2.setAttribute('onclick', 'inUah()')
+    createButtonDiv.appendChild(createButton2);
+
+    const createButton3 = document.createElement('button');
+    createButton3.textContent = 'AZN';
+    createButton3.setAttribute('onclick', "inAzn()")
+    createButtonDiv.appendChild(createButton3);
+}
+
+function task7Create() {
+    createDesc(`Введи сумму покупки`)
+    createInput();
+    createSubmit(7);
+}
+
+function task8Create() {
+    createDesc(`Введи сумму покупки`)
+    const createInputField1 = document.createElement('input');
+    createInputField1.setAttribute('id', 'input1');
+    createInputField1.className = 'minicaps';
+    createInputField1.placeholder = 'Сюда напиши длину окружности';
+    showAreaPlace.appendChild(createInputField1);
+    const createInputField2 = document.createElement('input');
+    createInputField2.setAttribute('id', 'input2');
+    createInputField2.className = 'minicaps';
+    createInputField2.placeholder = 'А сюда периметр квадрата';
+    showAreaPlace.appendChild(createInputField2);
+    createSubmit(8);
+}
+
+// =============   Task 1-5   ==================== 
 function task1() {
     let getting = +document.getElementById('input').value;
     switch (true) {
@@ -141,6 +185,51 @@ function task5() {
    ((getting[0] === getting [4]) && (getting[1] === getting [3])) ? 
         console.log('Да, это число-палиндром') :
         console.log('Не палиндром. Если ты вообще число ввёл')
+}
+
+// =============   Task 6  ====================
+function inEur() {
+    let getting = document.getElementById('input').value;
+    const course = 0.92396;
+    let converted = getting * course;
+    console.log(converted.toFixed(2));
+}
+function inUah() {
+    let getting = document.getElementById('input').value;
+    const course = 36.91;
+    let converted = getting * course;
+    console.log(converted.toFixed(2));
+}
+function inAzn() {
+    let getting = document.getElementById('input').value;
+    const course = 1.7;
+    let converted = getting * course;
+    console.log(converted.toFixed(2));
+}
+
+// =============   Task 7  ====================
+function task7() {
+    let getting = +document.getElementById('input').value;
+    switch (true) {
+        case (getting >= 0 && getting < 200):
+            console.log(getting);
+            break;
+        case (getting >= 200 && getting < 300):
+            console.log(getting + (getting * 0.03));
+            break;
+        case (getting >= 300 && getting < 500):
+            console.log(getting + (getting * 0.05));
+            break;
+        case (getting >= 500):
+            console.log(getting + (getting * 0.07));
+            break;
+        default: console.log('Ещё раз почитай, что от тебя хотят...')
+    }
+}
+
+function task8() {
+    let gettingCircle = +document.getElementById('input1').value;
+    let gettingSquare = +document.getElementById('input2').value;
 }
 // =============   Clear   ====================
 function clear() {
